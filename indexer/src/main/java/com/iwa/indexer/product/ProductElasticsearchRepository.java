c37f31a4d41d69f26f2c46ba896091802b0fbf4a
ZJ <zhijietan97@gmail.com>
2026-06-09 10:24:43 +0800

    chore: remove unnecessary modules

--- indexer/src/main/java/com/iwa/indexer/product/ProductElasticsearchRepository.java
+++
@@ -1,5 +0,0 @@
-package com.iwa.indexer.product;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ProductElasticsearchRepository extends ElasticsearchRepository<ProductDocument, String> {}
